885c12b7b20826e79a541733050dc1e748d44270
yanlonglv <lyl_office@163.com>
2021-01-10 19:09:49 +0800

    aff week06

--- Week06/rolling/rolling.go
+++ Week06/rolling/rolling.go
@@ -1,35 +1,33 @@
 package rolling
 
 import (
+	"math"
 	"sync"
 	"time"
 
 	"github.com/Go-000/Week06/window"
 )
 
-//RollingCounter ..
+//RollingCounter ...
 type RollingCounter interface {
 	Timespan() int
-	Add(int64)
-	Value() int64
+	Add(float64)
 	Min() float64
-	Max() float64
-	Avg() float64
-	Sum() float64
 }
 
 type rollingCounter struct {
 	mu             sync.RWMutex
 	size           int
-	window         *window.Window
+	w              *window.Window
 	lastUpdateTime time.Time
 	timeDuration   time.Duration
 	index          int
 }
 
+//NewRollingCounter ...
 func NewRollingCounter(w *window.Window, size int, timeDuration time.Duration) RollingCounter {
 	return &rollingCounter{
-		window:         w,
+		w:              w,
 		size:           size, //rolling 的时间长度
 		lastUpdateTime: time.Now(),
 		timeDuration:   timeDuration,
@@ -48,9 +46,25 @@ func (r *rollingCounter) Add(val float64) {
 		timespan = r.size
 	}
 	for i := 0; i < r.size; i++ {
-		r.window.ResetBucket(i)
-		offset = i
+		r.w.ResetBucket(i)
+		r.index = i
 	}
-	r.window.Add(r.index+1, val)
+	r.w.Add(r.index+1, val)
 
+}
+
+func (r *rollingCounter) Min() float64 {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	index := r.index + 1
+	if index == r.size {
+		index = 0
+	}
+	result := math.MaxFloat64
+	for _, v := range r.w.Widnow[index].Values {
+		if result < v {
+			result = v
+		}
+	}
+	return result
 }
